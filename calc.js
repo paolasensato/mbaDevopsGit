6799662a4f0d61e9b86762814631c25d220e690c
Paola Sensato Pasti <paolasensato03@gmail.com>
2023-11-17 21:50:42 -0300

    feat: add sub

--- calc.js
+++ calc.js
@@ -1,6 +1,8 @@
 const n1 = 10;
-const n2 = 15;
+const n2 = 20;
 
 const soma = n1 + n2;
+const sub = n1 - n2;
 
-console.log("soma ", soma);
+console.log("soma: ", soma);
+console.log("sub: ", sub);
